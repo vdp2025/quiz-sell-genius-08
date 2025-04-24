7ab90fbf52b023b4aaa2f304695e02aa139fd30f
vistasedeproposito <gracielegslouback@gmail.com>
2025-04-24 09:11:23 -0300

    chore: remove emblema de edição (EditorButton) do lovable

--- src/components/ui/EditorButton.tsx
+++ src/components/ui/EditorButton.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './button';
 import { Edit } from 'lucide-react';
-export const EditorButton = () => {
   return <Link to="/admin/resultado-editor">
       <Button className="">
         <Edit className="w-5 h-5 mr-2" />
