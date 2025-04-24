56567215ed724bc2493c8a9c7cdfb1796a6f13fa
vistasedeproposito <gracielegslouback@gmail.com>
2025-04-24 11:53:47 -0300

    fix: usa QuizBuilder no QuizBuilderPage

--- src/pages/QuizBuilderPage.tsx
+++ src/pages/QuizBuilderPage.tsx
@@ -1,9 +1,9 @@
 
 import React from 'react';
-import EnhancedQuizBuilder from '@/components/quiz-builder/EnhancedQuizBuilder';
+import QuizBuilder from '@/components/quiz-builder/QuizBuilder';
 
 const QuizBuilderPage: React.FC = () => {
-  return <EnhancedQuizBuilder />;
+  return <QuizBuilder />;
 };
 
 export default QuizBuilderPage;
