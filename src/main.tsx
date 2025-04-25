a439b8e529692c9942e207584e7614837700dd8d
vistasedeproposito <gracielegslouback@gmail.com>
2025-04-25 18:30:46 -0300

    Readded tailwindcss-animate dependency in package.json and package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import './index.css';
-import './index.css';
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-
